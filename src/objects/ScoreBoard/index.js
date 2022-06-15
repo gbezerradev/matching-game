734105273298b1bbd86a9d858594a5fc9ff585fc
Guilherme <gbezerradev@gmail.com>
2022-06-15 19:45:07 -0300

    :lipstick: Create styles of the PlayerScore component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -9,7 +9,7 @@ const ScoreBoard = () => {
       ${PlayerName('Player1')}
       ${PlayerScore(2)}
       ${VsPlayer()}
-      ${PlayerScore(1)}
+      ${PlayerScore(3)}
       ${PlayerName('Player2')}
     </header>
   `;
