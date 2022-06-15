d84afd996f2d2e9c16378113f4dacdf6004ba2cc
Guilherme <gbezerradev@gmail.com>
2022-06-15 18:58:36 -0300

    :construction: Create HTML of the PlayerScore component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,12 +1,15 @@
 import './style.css';
 import PlayerName from '../../components/PlayerName';
 import VsPlayer from '../../components/VsPlayer';
+import PlayerScore from '../../components/PlayerScore';
 
 const ScoreBoard = () => {
   return /*html*/ `
     <header class="score-board">
       ${PlayerName('Player1')}
+      ${PlayerScore(2)}
       ${VsPlayer()}
+      ${PlayerScore(1)}
       ${PlayerName('Player2')}
     </header>
   `;
